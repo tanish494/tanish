9651d0503a097524d3e8d58fff3f674ca488e816
tanish494 <129941618+tanish494@users.noreply.github.com>
2023-04-12 13:59:01 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	short t=1;
+	long l=1132;
+	signed k=10;
+	unsigned j=10;
+	printf("/n  t %d",t);
+	printf("/n  l %d",l);
+	printf("/n  k %d",k);
+	printf("/n  j %d",j);
+	getch();
+}
